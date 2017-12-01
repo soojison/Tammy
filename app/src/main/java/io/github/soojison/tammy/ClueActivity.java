4a21e9582e58c210d4e7abd626c9236aa62dc884
Sooji Son <soojison96@gmail.com>
2017-12-01 15:52:14 -0600

    the app technically works

--- app/src/main/java/io/github/soojison/tammy/ClueActivity.java
+++ app/src/main/java/io/github/soojison/tammy/ClueActivity.java
@@ -1,27 +1,98 @@
 package io.github.soojison.tammy;
 
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class ClueActivity extends AppCompatActivity {
+
+    @BindView(R.id.imgClue)
+    ImageView imgClue;
+
+    @BindView(R.id.btnContinue)
+    Button btnContinue;
+
+    boolean crashed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clue);
+        ButterKnife.bind(this);
+
+        crashed = false;
+
+        Bitmap it = BitmapFactory.decodeResource(getResources(), R.drawable.it);
+        Bitmap could = BitmapFactory.decodeResource(getResources(), R.drawable.could);
+        Bitmap have = BitmapFactory.decodeResource(getResources(), R.drawable.have);
+        Bitmap been = BitmapFactory.decodeResource(getResources(), R.drawable.been);
+
+        Bitmap[] arr = {it, could, have, been};
 
         // we always have to make sure that the intent we get has an extra that we put in
         if (getIntent().hasExtra("code")) {
             String code = getIntent().getStringExtra("code");
-
             //test to see if we have unpacked the correct one
-            Toast.makeText(this, code, Toast.LENGTH_SHORT).show();
+            int imageIndex = returnImgIndex(code);
+            if(imageIndex == 4) {
+                crashed = true;
+                View view = this.getWindow().getDecorView();
+                view.setBackgroundColor(000000);
+                imgClue.setVisibility(View.INVISIBLE);
+                btnContinue.setVisibility(View.INVISIBLE);
+            } else {
+                Bitmap bitmap = arr[imageIndex];
+                Glide.with(this).load(bitmap).into(imgClue);
+            }
         }
     }
 
-    // here we write a function that decides what happens based on the code
+    @Override
+    public void onBackPressed() {
+        if(crashed) {
+            new AlertDialog.Builder(this)
+                    .setTitle("It could have been")
+                    .setMessage("any of us")
+                    .setNegativeButton(android.R.string.no, null)
+                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
 
-    // todo: back button on certain codes so people can go back and enter more clues
-    // todo: black screen for a certain code so we can say that the app crashed!!!
+                        public void onClick(DialogInterface arg0, int arg1) {
+                            ClueActivity.super.onBackPressed();
+                        }
+                    }).create().show();
+        }
+    }
+
+    public int returnImgIndex(String code){
+        String[] possibilities = {"It","Could","Have", "Been", "Any of Us"};
+
+        for (int i = 0; i < possibilities.length; i++) {
+            if(possibilities[i].equalsIgnoreCase(code)) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    @OnClick(R.id.btnContinue)
+    public void closeActivity() {
+        finish();
+    }
+
+
+    // here we write a function that decides what happens based on the code
 }
--- app/src/main/java/io/github/soojison/tammy/CodeActivity.java
+++ app/src/main/java/io/github/soojison/tammy/CodeActivity.java
@@ -1,10 +1,18 @@
 package io.github.soojison.tammy;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.Display;
 import android.view.Gravity;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -21,6 +29,12 @@ public class CodeActivity extends AppCompatActivity {
 
     @BindView(R.id.etCode)
     EditText etCode;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        etCode.setText("");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,21 +57,34 @@ public class CodeActivity extends AppCompatActivity {
         return ret;
     }
 
+
+    public int returnImgIndex(String code){
+        String[] possibilities = {"It","Could","Have", "Been", "Any of Us"};
+
+        for (int i = 0; i < possibilities.length; i++) {
+            if(possibilities[i].equalsIgnoreCase(code)) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
     @OnClick(R.id.btnEnter)
     public void openActivity() {
+        String code = etCode.getText().toString().trim();
         if(TextUtils.isEmpty(etCode.getText().toString())) {
-            etCode.setError("yall can't leave this blank to get a clue");
-        } else if (isValidCode(etCode.getText().toString())){
+            etCode.setError("You must seek for the clue.");
+        } else if (isValidCode(code)){
             // Intents are how two activities (screens) communicate with each other.
             // So, this activity is creating an intent to start a new activity (clue activity)
             //      and we are putting extra information in the intent (the integer code)
             // Then we start the new activity!
             // Go to the ClueActivity.java class to see what happens there
             Intent intent = new Intent(CodeActivity.this, ClueActivity.class);
-            intent.putExtra("code", etCode.getText().toString());
+            intent.putExtra("code", code);
             startActivity(intent);
         } else {
-            etCode.setError("Not a valid clue");
+            etCode.setError("This is not a valid clue.");
         }
     }
 }
