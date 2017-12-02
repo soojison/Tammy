55bbc087417892d8c4b344dfdbfb8bd7db7daba5
Sooji Son <soojison96@gmail.com>
2017-12-02 13:02:46 -0600

    screaming this is so cheesy

---
+++ app/src/main/java/io/github/soojison/tammy/SplashActivity.java
@@ -0,0 +1,26 @@
+package io.github.soojison.tammy;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private final int SPLASH_LENGTH = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, CodeActivity.class));
+                finish();
+            }
+        }, SPLASH_LENGTH);
+
+    }
+}
